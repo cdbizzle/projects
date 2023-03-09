d479a1ff1a8979061f7c3a79300a8fa907496082
cdbizzle <71033480+cdbizzle@users.noreply.github.com>
2023-03-09 09:34:02 -0500

    Update linde.py

--- linde.py
+++ linde.py
@@ -1,41 +1,102 @@
-import csv
-import os
-import shutil
-
-# Define the directories to sort the photos into
-dir1 = 'directory1'
-dir2 = 'directory2'
-dir3 = 'directory3'
-
-# Create the directories if they don't exist already
-os.makedirs(dir1, exist_ok=True)
-os.makedirs(dir2, exist_ok=True)
-os.makedirs(dir3, exist_ok=True)
-
-# Read the mapping from the CSV file
-mapping = {}
-with open('photo_mapping.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        photo_name, dest_dir = row
-        mapping[photo_name] = dest_dir
-
-# Loop through each photo in the directory
-for photo_name in os.listdir('.'):
-    # If the file is not a photo, skip it
-    if not photo_name.endswith('.jpg'):
-        continue
-    
-    # If the photo is in the mapping, move it to the corresponding directory
-    if photo_name in mapping:
-        dest_dir = mapping[photo_name]
-        shutil.move(photo_name, dest_dir)
-        print(f"Moved {photo_name} to {dest_dir}")
-        
-# Save the mapping between photo names and destination directories in a CSV file named photo_mapping.csv.
-# The file should have two columns: the first column should contain the photo names (including the .jpg extension), 
-# and the second column should contain the corresponding destination directories (directory1, directory2, or directory3 in this case).  
-#
-# Place the CSV file in the same directory as the photos and the Python script. 
-# Then, run the script by typing python sort_photos.py in the command prompt or terminal, and hit Enter. 
-# The script should move the photos to the correct directories and print a message for each file that it moves.
+source /opt/vyatta/etc/functions/script-template
+set firewall name DMZ-to-LAN default-action 'drop'
+set firewall name DMZ-to-LAN enable-default-log
+set firewall name DMZ-to-LAN rule 1 action 'accept'
+set firewall name DMZ-to-LAN rule 1 state established 'enable'
+set firewall name DMZ-to-LAN rule 10 action 'accept'
+set firewall name DMZ-to-LAN rule 10 description 'WAZUH agent -> WAZUH server'
+set firewall name DMZ-to-LAN rule 10 destination address '172.16.200.10'
+set firewall name DMZ-to-LAN rule 10 destination port '1514,1515'
+set firewall name DMZ-to-LAN rule 10 protocol 'tcp'
+set firewall name DMZ-to-WAN default-action 'drop'
+set firewall name DMZ-to-WAN enable-default-log
+set firewall name DMZ-to-WAN rule 1 action 'accept'
+set firewall name DMZ-to-WAN rule 1 state established 'enable'
+set firewall name LAN-to-DMZ default-action 'drop'
+set firewall name LAN-to-DMZ enable-default-log
+set firewall name LAN-to-DMZ rule 1 action 'accept'
+set firewall name LAN-to-DMZ rule 1 state established 'enable'
+set firewall name LAN-to-DMZ rule 10 action 'accept'
+set firewall name LAN-to-DMZ rule 10 description 'Allow HTTP from LAN to NGINX'
+set firewall name LAN-to-DMZ rule 10 destination address '172.16.50.3'
+set firewall name LAN-to-DMZ rule 10 destination port '80'
+set firewall name LAN-to-DMZ rule 10 protocol 'tcp'
+set firewall name LAN-to-DMZ rule 20 action 'accept'
+set firewall name LAN-to-DMZ rule 20 description 'SSH from MGMT01 to DMZ'
+set firewall name LAN-to-DMZ rule 20 destination address '172.16.50.0/29'
+set firewall name LAN-to-DMZ rule 20 destination port '22'
+set firewall name LAN-to-DMZ rule 20 protocol 'tcp'
+set firewall name LAN-to-DMZ rule 20 source address '172.16.150.10'
+set firewall name LAN-to-WAN default-action 'drop'
+set firewall name LAN-to-WAN enable-default-log
+set firewall name LAN-to-WAN rule 1 action 'accept'
+set firewall name WAN-to-DMZ default-action 'drop'
+set firewall name WAN-to-DMZ enable-default-log
+set firewall name WAN-to-DMZ rule 1 action 'accept'
+set firewall name WAN-to-DMZ rule 1 state established 'enable'
+set firewall name WAN-to-DMZ rule 10 action 'accept'
+set firewall name WAN-to-DMZ rule 10 description 'Allow HTTP from WAN to DMZ'
+set firewall name WAN-to-DMZ rule 10 destination address '172.16.50.3'
+set firewall name WAN-to-DMZ rule 10 destination port '80'
+set firewall name WAN-to-DMZ rule 10 protocol 'tcp'
+set firewall name WAN-to-DMZ rule 20 action 'accept'
+set firewall name WAN-to-DMZ rule 20 description 'Allow SSH from WAN to Jump'
+set firewall name WAN-to-DMZ rule 20 destination address '172.16.50.4'
+set firewall name WAN-to-DMZ rule 20 destination port '22'
+set firewall name WAN-to-DMZ rule 20 protocol 'tcp'
+set firewall name WAN-to-LAN default-action 'drop'
+set firewall name WAN-to-LAN enable-default-log
+set firewall name WAN-to-LAN rule 1 action 'accept'
+set firewall name WAN-to-LAN rule 1 state established 'enable'
+set interfaces ethernet eth0 address '10.0.17.112/24'
+set interfaces ethernet eth0 description 'SEC350-02-WAN'
+set interfaces ethernet eth1 address '172.16.50.2/29'
+set interfaces ethernet eth1 description 'COLE-DMZ'
+set interfaces ethernet eth2 address '172.16.150.2/24'
+set interfaces ethernet eth2 description 'COLE-LAN'
+set nat destination rule 10 description 'HTTP to NGINX'
+set nat destination rule 10 destination port '80'
+set nat destination rule 10 inbound-interface 'eth0'
+set nat destination rule 10 protocol 'tcp'
+set nat destination rule 10 translation address '172.16.50.3'
+set nat destination rule 10 translation port '80'
+set nat destination rule 20 description 'WAN to JUMP'
+set nat destination rule 20 destination port '22'
+set nat destination rule 20 inbound-interface 'eth0'
+set nat destination rule 20 protocol 'tcp'
+set nat destination rule 20 translation address '172.16.50.4'
+set nat destination rule 20 translation port '22'
+set nat source rule 10 description 'NAT from DMZ to WAN'
+set nat source rule 10 outbound-interface 'eth0'
+set nat source rule 10 source address '172.16.50.0/29'
+set nat source rule 10 translation address 'masquerade'
+set nat source rule 20 description 'NAT from LAN to WAN'
+set nat source rule 20 outbound-interface 'eth0'
+set nat source rule 20 source address '172.16.150.0/24'
+set nat source rule 20 translation address 'masquerade'
+set nat source rule 30 description 'NAT from MGMT to WAN'
+set nat source rule 30 outbound-interface 'eth0'
+set nat source rule 30 source address '172.16.200.0/28'
+set nat source rule 30 translation address 'masquerade'
+set protocols rip interface eth2
+set protocols rip network '172.16.50.0/29'
+set protocols static route 0.0.0.0/0 next-hop 10.0.17.2
+set service dns forwarding allow-from '172.16.50.0/29'
+set service dns forwarding allow-from '172.16.150.0/24'
+set service dns forwarding listen-address '172.16.50.2'
+set service dns forwarding listen-address '172.16.150.2'
+set service dns forwarding system
+set service ssh listen-address '172.16.150.2'
+set system host-name 'edge01-cole'
+set system name-server '10.0.17.2'
+set zone-policy zone DMZ from LAN firewall name 'LAN-to-DMZ'
+set zone-policy zone DMZ from WAN firewall name 'WAN-to-DMZ'
+set zone-policy zone DMZ interface 'eth1'
+set zone-policy zone LAN from DMZ firewall name 'DMZ-to-LAN'
+set zone-policy zone LAN from WAN firewall name 'WAN-to-LAN'
+set zone-policy zone LAN interface 'eth2'
+set zone-policy zone WAN from DMZ firewall name 'DMZ-to-WAN'
+set zone-policy zone WAN from LAN firewall name 'LAN-to-WAN'
+set zone-policy zone WAN interface 'eth0'
+commit
+save
